a5e8def10d1fb0b711fddbff3431f848e9b58481
Domenico Pagano <dpaganoh@gmail.com>
2025-10-29 19:12:52 -0500

    mario crouch resuelto! -> Player.js

--- src/entities/Player.js
+++ src/entities/Player.js
@@ -12,8 +12,10 @@ export default class Player {
         this.size = 'small';
         this.isTransforming = false;
         this.invulnerable = false;
+        this.isCrouching = false;
 
-        this.sprite = scene.physics.add.sprite(x, y, 'mario-small').setSize(10, 16);
+        this.sprite = scene.physics.add.sprite(x, y, 'mario-small');
+        this.setBodySize(10, 16, 3, 0);
 
         this.sprite.setCollideWorldBounds(true);
 
@@ -38,11 +40,13 @@ export default class Player {
         anims.create({ key: 'big-idle', frames: [{ key: 'mario-big', frame: 0}], frameRate: 1});
         anims.create({ key: 'big-walk', frames: anims.generateFrameNumbers('mario-big', {start: 1, end: 2}), frameRate: 8, repeat: -1 });
         anims.create({ key: 'big-jump', frames: [{ key: 'mario-big', frame: 5}], frameRate: 1});
+        anims.create({ key: 'big-crouch', frames: [{ key: 'mario-big', frame: 6 }], frameRate: 1, repeat: -1})
 
         // Mario Fire
         anims.create({ key: 'fire-idle', frames: [{ key: 'mario-fire', frame: 0}], frameRate: 1});
         anims.create({ key: 'fire-walk', frames: anims.generateFrameNumbers('mario-fire', {start: 1, end: 2}), frameRate: 8, repeat: -1 });
         anims.create({ key: 'fire-jump', frames: [{ key: 'mario-fire', frame: 5}], frameRate: 1});
+        anims.create({ key: 'fire-crouch', frames: [{ key: 'mario-fire', frame: 6 }], frameRate: 1, repeat: -1})
 
         // Mario dies
         anims.create({ key: 'small-die', frames: [{ key: 'mario-small', frame: 6 }], frameRate: 1 });
@@ -94,24 +98,16 @@ export default class Player {
             frameRate: 12,
             repeat: 0,
         })
+
     }
 
     get body(){ 
         return this.sprite.body 
     };
 
-    update(){
-        if(!this.isTransforming) this.playerControl()
+    update() {
+        if(!this.isTransforming) this.playerControl();
         this.playerAnims();
-        this.size === 'small' && this.sprite.setSize(
-            this.body.onFloor() ? 10 : 8, 
-            this.sprite.body.height
-        );
-        
-        (this.size === 'big' || this.size === 'fire') && this.sprite.setSize(
-            this.body.onFloor() ? 14 : 11,
-            this.sprite.body.height
-        );
 
         if(Phaser.Input.Keyboard.JustDown(this.shootKey)){
             this.shootFireball();
@@ -125,14 +121,28 @@ export default class Player {
     playerControl(){
         const {left, right, space} = this.cursors;
 
-        if(left.isDown){
+        if(left.isDown && !this.isCrouching){
             this.sprite.setAccelerationX(-acceleration);
             this.sprite.setFlipX(true);
-        } else if(right.isDown){
+        } else if(right.isDown && !this.isCrouching){
             this.sprite.setAccelerationX(acceleration);
             this.sprite.setFlipX(false);
         } else {
             this.sprite.setAccelerationX(0);
+        }
+
+        // crouch
+        if ((this.size === 'big' || this.size === 'fire') && this.cursors.down.isDown && this.body.onFloor()) {
+            if (!this.isCrouching) {
+                this.isCrouching = true;
+                this.sprite.body.setSize(14, 20);
+                this.sprite.body.setOffset(1, 12);
+            }
+        } else if (this.isCrouching) {
+            // dejar de agacharse
+            this.isCrouching = false;
+            this.sprite.body.setSize(14, 32);
+            this.sprite.body.setOffset(1, 0);
         }
 
         // jump
@@ -165,14 +175,24 @@ export default class Player {
         } else {
             this.sprite.play(`${prefix}-idle`, true);
         }
+
+        // crouch anim
+        if (this.isCrouching) {
+            const prefix = this.size;
+            this.sprite.play(`${prefix}-crouch`, true);
+            return; // evita que entre en idle o walk
+        }
     }
 
     // setBodySize: Ajusta el tamaño y offset del cuerpo físico manteniendo los pies del sprite en la misma posición (no se hunde ni flota).
-    setBodySize(width, height, offsetX, offsetY = 0) {
+    setBodySize(width, height, offsetX = 0, offsetY = 0) {
         const oldBottom = this.sprite.body.bottom;
-        this.sprite.setSize(width, height).setOffset(offsetX, offsetY);
+        this.sprite.body.setSize(width, height);
+        this.sprite.body.setOffset(offsetX, offsetY);
         this.sprite.y = oldBottom - this.sprite.body.halfHeight;
     }
+
+
 
     grow() {
         if (this.size !== 'small' || this.isTransforming) return;
@@ -184,7 +204,7 @@ export default class Player {
         this.sprite.body.allowGravity = false; // opcional: congelar en el aire
         this.sprite.anims.stop(); // detener cualquier animación previa
 
-        this.setBodySize(14, 32, true);   // hitbox grande
+        this.setBodySize(14, 32, 1, 0);
         this.sprite.play('transform-grow');
     
         this.sprite.once(Phaser.Animations.Events.ANIMATION_COMPLETE, () => {
@@ -200,16 +220,11 @@ export default class Player {
         
         this.isTransforming = true;
         
-        const oldBottom = this.sprite.body.bottom; // guardamos los pies actuales
-        
         this.sprite.play('transform-shrink');
         this.sprite.once(Phaser.Animations.Events.ANIMATION_COMPLETE, () => {
             this.size = 'small';
             this.sprite.setTexture('mario-small');
-            this.sprite.setSize(10, 16);
-        
-            // mantener pies en el suelo:
-            this.sprite.y = oldBottom - this.sprite.body.halfHeight;
+            this.setBodySize(10, 16, 3, 0);
         
             this.isTransforming = false;
         });
